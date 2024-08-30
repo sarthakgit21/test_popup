2e83813e455d7df73059b010ee25f2b50f7cacee
sarthakgit21 <sarthaksethji92@gmail.com>
2024-08-30 11:44:30 +0530

    src

--- public/script.js
+++ public/script.js
@@ -1,29 +1,29 @@
 // Function to show a popup
 function showPopup(message) {
-    alert(message);
+  alert(message);
+}
+
+// Function to show a popup when the website is loaded
+function onPageLoad() {
+  showPopup("Welcome to the website!");
+}
+
+// Function to check if the user has scrolled to the bottom of the page
+function checkScrollPosition() {
+  // Get the total scrollable height
+  // console.log("scrooled");
+  const scrollableHeight = document.documentElement.scrollHeight - 10;
+  // Get the current scroll position
+  const currentScrollPosition = window.scrollY + window.innerHeight;
+  console.log(scrollableHeight, currentScrollPosition);
+  // If the current scroll position is at the bottom of the page, show the popup
+  if (currentScrollPosition >= scrollableHeight) {
+    showPopup("You have reached the end of the page!");
   }
-  
-  // Function to show a popup when the website is loaded
-  function onPageLoad() {
-    showPopup("Welcome to the website!");
-  }
-  
-  // Function to check if the user has scrolled to the bottom of the page
-  function checkScrollPosition() {
-    // Get the total scrollable height
-    const scrollableHeight = document.documentElement.scrollHeight;
-    // Get the current scroll position
-    const currentScrollPosition = window.scrollY + window.innerHeight;
-  
-    // If the current scroll position is at the bottom of the page, show the popup
-    if (currentScrollPosition >= scrollableHeight) {
-      showPopup("You have reached the end of the page!");
-    }
-  }
-  
-  // Add an event listener for the page load event
-  window.onload = onPageLoad;
-  
-  // Add an event listener for the scroll event
-  window.addEventListener('scroll', checkScrollPosition);
-  
+}
+
+// Add an event listener for the page load event
+window.onload = onPageLoad;
+
+// Add an event listener for the scroll event
+window.addEventListener("scroll", checkScrollPosition);
